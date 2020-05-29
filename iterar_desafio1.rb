b1bfe3ed33386db3c29afdac966c600d4de74e57
Romi Maccarini <romi@clay.cl>
2020-05-29 02:15:48 -0400

    resultados comentados

--- iterar_desafio1.rb
+++ iterar_desafio1.rb
@@ -1,3 +1,5 @@
 a = [1, 9 ,2, 10, 3, 7, 4, 6]
-a.map{|e| e.to_f + 1 }.select{ |e| e >= 5}.count
-a.inject{|sum, x| sum += x }
+
+a.map{|e| e.to_f + 1 }.select{ |e| e > 5}.count # 4
+a.inject{|sum, x| sum += x } # 42
+
